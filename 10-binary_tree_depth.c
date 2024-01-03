2ff32cd83ee2ea40ca2d4bd1d17501d27347aafb
Peter-Mwangi254 <pm6093628@gmail.com>
2024-01-03 23:37:55 +0300

    binary trees solution

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,6 +1,22 @@
 #include "binary_trees.h"
 
+/**
+* binary_tree_depth - function that measures the depth of a
+* node in a binary tree
+* @tree: pointer to the node to measure the depth
+* Return: depth of node or 0 if tree is NULL
+*/
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-    
+size_t depth_node = 0;
+if (tree == NULL)
+{
+return (0);
+}
+while (tree->parent)
+{
+depth_node += 1;
+tree = tree->parent;
+}
+return (depth_node);
 }
--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,3 +1,22 @@
 #include "binary_trees.h"
 
+/**
+* binary_tree_height - measures the height of a binary tree
+* @tree: tree to measure the height of
+* Return: height of the tree
+* 0 if tree is NULL
+*/
 size_t binary_tree_height(const binary_tree_t *tree)
+{
+{
+if (tree == NULL)
+{
+return (0);
+}
+
+size_t left_height = binary_tree_height(tree->left);
+size_t right_height = binary_tree_height(tree->right);
+
+return (1 + (left_height > right_height ? left_height : right_height));
+}
+}
