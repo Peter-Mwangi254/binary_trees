835eec5724e124d15933be6a429e00d8e70cc8ed
Peter-Mwangi254 <pm6093628@gmail.com>
2024-01-04 00:50:24 +0300

    function that measures the balance factor of a binary tree

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -32,7 +32,9 @@ int binary_tree_balance(const binary_tree_t *tree)
 int left_height;
 int right_height;
 if (tree == NULL)
+{
 return (0);
+}
 left_height = binary_tree_height(tree->left);
 right_height = binary_tree_height(tree->right);
 return (left_height - right_height);
