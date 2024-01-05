eb6ee35856185fa38d9784f9bfe899a187cd5ec1
Peter-Mwangi254 <pm6093628@gmail.com>
2024-01-05 23:49:16 +0300

    update

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,14 +8,15 @@
 */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+size_t left_height, right_height;
 {
 if (tree == NULL)
 {
 return (0);
 }
 
-size_t left_height = binary_tree_height(tree->left);
-size_t right_height = binary_tree_height(tree->right);
+left_height = binary_tree_height(tree->left);
+right_height = binary_tree_height(tree->right);
 
 return (1 + (left_height > right_height ? left_height : right_height));
 }
